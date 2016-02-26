bb9e0b2b98fe0c49d5e01c1001be67d28098b17d
Sean Scalabrini <sscalabrini@digitallumens.com>
2016-02-26 18:40:59 -0500

    go away eve

--- app/actions/NavbarActions.js
+++ app/actions/NavbarActions.js
@@ -6,36 +6,8 @@ class NavbarActions {
     this.generateActions(
       'updateOnlineUsers',
       'updateAjaxAnimation',
-      'updateSearchQuery',
-      'getCharacterCountSuccess',
-      'getCharacterCountFail',
-      'findCharacterSuccess',
-      'findCharacterFail'
+      'updateSearchQuery'
     );
-  }
-
-  findCharacter(payload) {
-    $.ajax({
-      url: '/api/characters/search',
-      data: { name: payload.searchQuery }
-    })
-      .done((data) => {
-        assign(payload, data);
-        this.actions.findCharacterSuccess(payload);
-      })
-      .fail(() => {
-        this.actions.findCharacterFail(payload);
-      });
-  }
-
-  getCharacterCount() {
-    $.ajax({ url: '/api/characters/count' })
-      .done((data) => {
-        this.actions.getCharacterCountSuccess(data)
-      })
-      .fail((jqXhr) => {
-        this.actions.getCharacterCountFail(jqXhr)
-      });
   }
 }
 
