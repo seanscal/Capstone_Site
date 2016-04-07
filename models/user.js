81bcd3a96a37fb751db5887253ef9a0f5543101c
Sean Scalabrini <sscalabrini@digitallumens.com>
2016-04-07 14:01:56 -0400

    default

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ var userSchema = new mongoose.Schema({
   proximity: Number,
   durationNotif: Number,
   updateTimeStamp: Number,
-  password: String
+  password: {type: String, default:null}
 });
 
 module.exports = mongoose.model('User', userSchema);
